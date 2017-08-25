ffdae468968245a53d114de067d7c3176aaed37d
Lenovo <fengjie_global@atguigu.com>
2017-08-25 10:25:54 +0800

    test all flow

--- src/main/java/com/atguigu/jenkins/test/Good.java
+++ src/main/java/com/atguigu/jenkins/test/Good.java
@@ -3,7 +3,7 @@ package com.atguigu.jenkins.test;
 public class Good {
 	
 	public String getGoodMessage() {
-		return "good message[test hook]";
+		return "good message[test hook][test all flow]";
 	}
 
 }
